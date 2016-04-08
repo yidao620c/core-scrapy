c7dee9ea83fb761792aa99371c93306852bb6f64
XiongNeng <yidao620@gmail.com>
2016-04-08 16:18:51 +0800

    正则式修正

--- coolscrapy/spiders/test_spider.py
+++ coolscrapy/spiders/test_spider.py
@@ -18,9 +18,10 @@ class TestSpider(scrapy.Spider):
 
     def parse(self, response):
         body = response.xpath('//div[@class="PubDate"]/text()').extract()
-        body2 = body.encode('utf-8')
+        body1 = ''.join(body).strip()
+        body2 = body1.encode('utf-8')
         pat4 = re.compile(r'\d{4}年\d{2}月\d{2}日')
-        if (re.search(pat4, body)):
+        if (re.search(pat4, body1)):
             print('body...')
         if (re.search(pat4, body2)):
             print('body2222222222222')
