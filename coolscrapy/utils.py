ba8b1c5bf9fae90bec8a17e26b5d4423004428a1
XiongNeng <yidao620@gmail.com>
2016-04-08 16:01:49 +0800

    keyerror修正

--- coolscrapy/utils.py
+++ coolscrapy/utils.py
@@ -196,9 +196,10 @@ def parse_text(extract_texts, rule_name, attr_name):
     @param rule_name: 规则名称
     @param attr_name: 属性名
     """
-    custom_func = globals()["%s_%s" % (rule_name, attr_name)]
-    if custom_func:
-        return custom_func(extract_texts)
+
+    custom_func = "%s_%s" % (rule_name, attr_name)
+    if custom_func in globals():
+        return globals()[custom_func](extract_texts)
     return '\n'.join(extract_texts).strip() if extract_texts else ""
 
 
