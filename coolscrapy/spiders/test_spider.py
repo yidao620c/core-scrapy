8ea1749ddaec1a17861a8a3d4f24030787ac694d
XiongNeng <yidao620@gmail.com>
2016-04-08 16:16:54 +0800

    正则式修正

--- coolscrapy/spiders/test_spider.py
+++ coolscrapy/spiders/test_spider.py
@@ -6,22 +6,28 @@ Desc :
 """
 import logging
 import scrapy
+import re
 
 
 class TestSpider(scrapy.Spider):
     name = "test"
-    allowed_domains = ["huxiu.com"]
+    allowed_domains = ["oschina.net"]
     start_urls = [
-        "http://www.huxiu.com/article/144610/1.html?f=index_feed_article"
+        "http://www.oschina.net/news/72297/csharp-7-features-preview"
     ]
 
     def parse(self, response):
-        body = response.xpath('//div[@id="article_content"]/p/text()').extract()
-        if body:
-            for b in body:
-                print(b)
-        print('---------------result----------------')
-        result = '\n'.join(body) if body else ""
-        logging.info(result)
+        body = response.xpath('//div[@class="PubDate"]/text()').extract()
+        body2 = body.encode('utf-8')
+        pat4 = re.compile(r'\d{4}年\d{2}月\d{2}日')
+        if (re.search(pat4, body)):
+            print('body...')
+        if (re.search(pat4, body2)):
+            print('body2222222222222')
+        logging.info('---------------success----------------')
 
-
+if __name__ == '__main__':
+    body = '发布于： 2016年04月08日'
+    pat4 = re.compile(r'\d{4}年\d{2}月\d{2}日')
+    if (re.search(pat4, body)):
+        print(re.search(pat4, body).group())
