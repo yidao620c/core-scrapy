078a998cb4534661b1c4a7a72a8821d93efb0ab4
yidao620 <yidao620@gmail.com>
2017-11-24 18:15:20 +0800

    增加一个烟草条形码的爬虫例子

--- coolscrapy/spiders/tobacco_spider.py
+++ coolscrapy/spiders/tobacco_spider.py
@@ -8,8 +8,8 @@ CREATE TABLE `t_tobacco` (
   `id`                        BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '主键ID',
   `product_name`              VARCHAR(32)  COMMENT '产品名称',
   `brand`                     VARCHAR(32)  COMMENT '品牌',
-  `product_type`              VARCHAR(16)  COMMENT '产品类型',
-  `package_spec`              VARCHAR(32)  COMMENT '包装规格',
+  `product_type`              VARCHAR(32)  COMMENT '产品类型',
+  `package_spec`              VARCHAR(64)  COMMENT '包装规格',
   `reference_price`           VARCHAR(32)  COMMENT '参考价格',
   `manufacturer`              VARCHAR(32)  COMMENT '生产厂家',
   `pics`                      VARCHAR(255) COMMENT '图片URL',
@@ -45,7 +45,7 @@ class TobaccoSpider(Spider):
     allowed_domains = ["etmoc.com"]
     base_url = "http://www.etmoc.com/market/Brandlist.asp"
     start_urls = [
-        base_url
+        "http://www.etmoc.com/market/Brandlist.asp?page=86&worded=&temp="
     ]
     pics_pre = 'http://www.etmoc.com/'
 
@@ -54,9 +54,10 @@ class TobaccoSpider(Spider):
         for item in self.parse_page(response):
             yield item
         # 找下一页链接递归爬
-        next_url = response.xpath('//a[text()="【下一页】"]/@href').extract()[0]
-        self.logger.info('+++++++++++next_url++++++++++=' + self.base_url + next_url)
-        yield Request(url=self.base_url + next_url, meta={'ds': "ds"}, callback=self.parse)
+        next_url = tx(response.xpath('//a[text()="【下一页】"]/@href'))
+        if next_url:
+            self.logger.info('+++++++++++next_url++++++++++=' + self.base_url + next_url)
+            yield Request(url=self.base_url + next_url, meta={'ds': "ds"}, callback=self.parse)
 
     def parse_page(self, response):
         self.logger.info('Hi, this is a page = %s', response.url)
@@ -71,10 +72,11 @@ class TobaccoSpider(Spider):
             barcode1 = tx(each_row.xpath('td[2]/p[3]/text()'))
             barcode2 = tx(each_row.xpath('td[2]/p[4]/text()'))
             item['product'] = "{}/{}/{}/{}".format(product_name, brand, barcode1, barcode2)
-            item['product_type'] = each_row.xpath('td[3]/text()').extract()[0]
-            item['package_spec'] = each_row.xpath('td[4]/text()').extract()[0]
-            item['reference_price'] = each_row.xpath('td[5]/span/text()').extract()[0]
-            item['manufacturer'] = each_row.xpath('td[6]/text()').extract()[0]
+            item['product_type'] = tx(each_row.xpath('td[3]/text()'))
+            item['package_spec'] = tx(each_row.xpath('td[4]/text()'))
+            item['reference_price'] = tx(each_row.xpath('td[5]/span/text()'))
+            # 生产厂家有可能包含<a>链接，我取里面的文本，使用//text()
+            item['manufacturer'] = tx(each_row.xpath('td[6]//text()'))
             self.logger.info("pics={},product={},product_type={},package_spec={},"
                              "reference_price={},manufacturer={}".format(
                 item['pics'], item['product'], item['product_type'], item['package_spec']
